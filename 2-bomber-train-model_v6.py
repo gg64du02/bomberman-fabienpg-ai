db8d5c736a284a2ec59c1e818342e2f108ee6330
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2018-06-18 08:44:34 +0200

    Got the training working with Keras

---
+++ 2-bomber-train-model_v6.py
@@ -0,0 +1,201 @@
+import numpy as np
+from grabscreen import grab_screen
+import cv2
+import time
+import os
+import pandas as pd
+from tqdm import tqdm
+from collections import deque
+# from models import inception_v3 as googlenet
+from random import shuffle
+# import tensorflow as tf
+
+
+from keras.models import Sequential
+from keras.models import Model
+from keras.layers import Input, Dense
+from keras import optimizers
+from keras import losses
+import keras
+
+
+# https://keras.io/losses/
+# https://keras.io/optimizers/
+# https://keras.io/models/model/
+
+
+# FILE_I_END = 19
+FILE_I_END = 1
+
+# WIDTH = 480
+# HEIGHT = 270
+WIDTH = int( 640 / 2 )
+HEIGHT = int( 480 / 2 )
+# 320 * 240
+
+LR = 1e-3
+# EPOCHS = 30
+EPOCHS = 1
+
+MODEL_NAME = ''
+PREV_MODEL = ''
+
+LOAD_MODEL = True
+
+# outer NN variables ?
+e1 =     0
+d1 =     0
+s1 =     0
+f1 =     0
+ctrl1 =  0
+shift1 = 0
+
+# default player keyboard binding
+e =     [1,0,0,0,0,0]
+d =     [0,1,0,0,0,0]
+s =     [0,0,1,0,0,0]
+f =     [0,0,0,1,0,0]
+ctrl =  [0,0,0,0,1,0]
+shift = [0,0,0,0,0,1]
+
+# For a single-input model with 10 classes (categorical classification):
+
+# https://keras.io/getting-started/sequential-model-guide/
+# 320 * 240 = 76800
+model = Sequential()
+# 32 is probably the batch size
+model.add(Dense(32, activation='relu', input_dim=76800))
+# 6 is probably the number of classes
+model.add(Dense(6, activation='softmax'))
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# # Generate dummy data
+# import numpy as np
+# data = np.random.random((1000, 100))
+# labels = np.random.randint(10, size=(1000, 1))
+#
+# # Convert labels to categorical one-hot encoding
+# one_hot_labels = keras.utils.to_categorical(labels, num_classes=10)
+#
+# # Train the model, iterating on the data in batches of 32 samples
+# model.fit(data, one_hot_labels, epochs=10, batch_size=32)
+
+#
+# if LOAD_MODEL:
+#     model.load(PREV_MODEL)
+#     print('We have loaded a previous model!!!!')
+
+# iterates through the training files
+
+for e in range(EPOCHS):
+    # data_order = [i for i in range(1,FILE_I_END+1)]
+    data_order = [i for i in range(1, FILE_I_END + 1)]
+    shuffle(data_order)
+    for count, i in enumerate(data_order):
+
+        try:
+            # file_name = 'J:/phase10-random-padded/training_data-{}.npy'.format(i)
+            file_name = './phase7-larger-color/training_data-{}.npy'.format(i)
+
+            # full file info
+            train_data = np.load(file_name)
+            print('training_data-{}.npy'.format(i), len(train_data))
+
+            # shuffle(train_data)
+
+            print("count:",1)
+
+            train = train_data[:-50]
+            test = train_data[-50:]
+
+            print("count:",2)
+
+            X = np.array([i[0] for i in train]).reshape(-1, WIDTH, HEIGHT, 3)
+            Y = [i[1] for i in train]
+            print("X.shape:",X.shape)
+            print("len(Y):", len(Y))
+
+            test_x = np.array([i[0] for i in test]).reshape(-1, WIDTH, HEIGHT, 3)
+            # test_y = [i[1] for i in test]
+
+            test_y = [i[1] for i in test]
+            # test_y2 = []
+            test_y2 = np.zeros((50, 1))
+            tmp_i = 0
+            print("count1test")
+            for i in test_y:
+                tmp_ii = 0
+                # print("count2test")
+                for ii in i:
+                    # print("count3test")
+                    # print("ii:",ii)
+                    if(ii==1):
+                        # print("count4test")
+                        test_y2[tmp_i,0] = tmp_ii
+                    tmp_ii +=1
+                tmp_i +=1
+            # print("test_y2:",test_y2)
+            print("test_y2.shape:",test_y2.shape)
+
+
+            print("test_x:",test_x.shape)
+            print("len(test_y):",len(test_y))
+
+
+            # data = np.random.random((1000, 100))
+            # labels = np.random.randint(10, size=(1000, 1))
+
+            # Generate dummy data
+            # data = test_x[0,:,:,0]
+            # data = test_x[0,:,:,0].reshape((WIDTH*HEIGHT,))
+            data = test_x[:,:,:,0].reshape((50,WIDTH*HEIGHT))
+            print("data.shape:",data.shape)
+            # labels = np.asarray(test_y)
+            labels = np.asarray(test_y2)
+            print("len(labels):",len(labels))
+            print("labels.shape:",labels.shape)
+
+            # labels = test_y.reshape((WIDTH*HEIGHT,))
+            # print("labels.shape):",labels.shape)
+
+
+            # from the website
+            # data = np.random.random((1000, 76800))
+            # labels = np.random.randint(6, size=(1000, 1))
+            # print("data:",data.shape)
+            # print("labels:",labels.shape)
+
+            print("count:",3)
+
+            # data1 = data.transpose()
+
+            print("count:",4)
+
+            # Convert labels to categorical one-hot encoding
+            one_hot_labels = keras.utils.to_categorical(labels, num_classes=6)
+            # print(one_hot_labels)
+            print("one_hot_labels:",one_hot_labels.shape)
+
+            print("count:",5)
+
+            # Train the model, iterating on the data in batches of 32 samples
+            model.fit(data, one_hot_labels, epochs=10, batch_size=32)
+
+            print("count:",6)
+
+            if count % 10 == 0:
+                print('SAVING MODEL!')
+                # model.save(MODEL_NAME)
+                model.save('./test.tfmodel')
+
+        except Exception as e:
+            print(str(e))
+            print(sys.exc_info())
+
+
+#
+
+# tensorboard --logdir=foo:J:/phase10-code/log
+
--- TODO.md
+++ TODO.md
@@ -1,3 +1,8 @@
+# TODO: make a balancing data script
+
+# TODO: make a specific config (for the game) to only use (e,s,d,f and ctrl)
+ to get more balanced data after capture
+
 # TODO: try a training
 
 # TODO: fixed and make equal inputs for each keys (might be hard for the shift key)
@@ -13,3 +18,7 @@
 ## ======================================
 
 # DONE: Initial commit
+
+# DONE: make a collecting data script
+
+# DONE: make a training script (with Keras)
