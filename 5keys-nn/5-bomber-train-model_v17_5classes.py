69a9be78a195ab3238328a20af690d7ef2e4f5d7
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2018-07-20 00:04:58 +0200

    5keys-nn: cleaning v17

--- 5keys-nn/5-bomber-train-model_v17_5classes.py
+++ 5keys-nn/5-bomber-train-model_v17_5classes.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-# # forcing tf-cpu put this before tf and keras import
-# import os
-# os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+# forcing tf-cpu put this before tf and keras import
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 import cv2
 import time
@@ -50,13 +50,6 @@ HEIGHT = int( 480 / 2 )
 # 320 * 240
 
 LR = 1e-3
-# LR = 1e-4
-# LR = 1e-4
-# EPOCHS = 30
-# EPOCHS = 1
-# EPOCHS = 10
-# EPOCHS = 120
-# EPOCHS = 44
 EPOCHS = 1
 
 MODEL_NAME = 'bomberman-nn-keras_v16_5classes.h5'
@@ -83,30 +76,6 @@ shift = [0,0,0,0,0,1]
 # For a single-input model with 10 classes (categorical classification):
 
 from keras.layers.core import Activation
-
-# # # ============================================================
-# # https://keras.io/getting-started/sequential-model-guide/
-# # 320 * 240 = 76800
-# model = Sequential()
-# # Input tensor for sequences of 32 timesteps,
-# model.add(Dense(200, activation='relu', input_dim=76800))
-# # model.add(Activation('relu'));
-# # grid is 15*20 32 pixels
-# # make it so it would be 30*40
-# # /64
-# model.add(Dense(1200, activation='relu', input_dim=1200))
-# keras.layers.Dropout(0.5)
-# # model.add(Activation('relu'));
-# # 6 is probably the number of classes
-# model.add(Dense(5, activation='softmax'))
-# # model.add(Activation('relu'));
-# model.compile(optimizer='rmsprop',
-#               loss='categorical_crossentropy',
-#               metrics=['accuracy'])
-# # model.compile(optimizer='adagrad',
-# #               loss='categorical_crossentropy',
-# #               metrics=['accuracy'])
-# # # ============================================================
 
 
 model = Sequential()
@@ -147,12 +116,6 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 tensorboard = TensorBoard(log_dir="logs/STAGE1")
-
-
-# a = Input(shape=(76800,))
-# b = Dense(32)(a)
-# model = Model(inputs=a, outputs=b)
-
 
 
 if LOAD_MODEL:
@@ -238,25 +201,11 @@ def generate_arrays_from_folder(folder):
                 print("len(labels):", len(labels))
                 print("labels.shape:", labels.shape)
 
-                # from the website
-                # data = np.random.random((1000, 76800))
-                # labels = np.random.randint(6, size=(1000, 1))
-                # print("data:",data.shape)
-                # print("labels:",labels.shape)
-
                 # Convert labels to categorical one-hot encoding
                 one_hot_labels = keras.utils.to_categorical(labels, num_classes=5)
                 print("one_hot_labels:", one_hot_labels.shape)
 
                 print("data.shape:",data.shape)
-
-                # Train the model, iterating on the data in batches of 32 samples
-                # model.fit(data, one_hot_labels, epochs=10, batch_size=32)
-
-                # model.fit(data, one_hot_labels, epochs=1, verbose=0,steps_per_epoch=55)
-
-                # yield(data,one_hot_labels)
-
 
                 # yield(data/255, one_hot_labels)
 
@@ -267,51 +216,6 @@ def generate_arrays_from_folder(folder):
                 print("len(one_hot_labels[0]):",len(one_hot_labels[0]))
                 # len(one_hot_labels[0]): 5
 
-                # # nope: ValueError: Error when checking input: expected conv2d_1_input to have 4 dimensions, but got array with shape (240, 320, 3)
-                # for i in range(len(one_hot_labels)):
-                #     yield (train_data[i,0], train_data[i,1])
-                # # nope:  ValueError: Error when checking target: expected dense_2 to have shape (5,) but got array with shape (1,)
-                # for i in range(len(one_hot_labels)):
-                #     yield (train_data[i,0].reshape(-1, 240, 320, 3), np.asarray(train_data[i,1]))
-                # nope:
-                # for i in range(len(one_hot_labels)):
-                #     yield (train_data[i,0].reshape(-
-
-                print()
-
-                # # next try:(np.asarray([[ii] for ii in train_data[i, 1]]).reshape(5, -1)).shape
-                # for i in range(len(one_hot_labels)):
-                #     print("\npicture:",i,"\n")
-                #     yield (train_data[i,0].reshape(-1, 240, 320, 3), np.asarray([[ii] for ii in train_data[i, 1]]).reshape( -1,5))
-                #     # model.fit(train_data[i, 0].reshape(-1, 240, 320, 3),
-                #     #        np.asarray([[ii] for ii in train_data[i, 1]]).reshape(-1, 5),shuffle=True, verbose=1,
-                #     #           callbacks=[tensorboard])
-
-                # nope: ValueError: Error when checking model input: the list of Numpy arrays that you are passing to your model
-                # is not the size the model expected. Expected to see 1 array(s), but instead got the following list of 3225 arrays: [array([[[148, 168, 168],
-                # yield (train_data[:,0].reshape(len(train_data[:,0])).tolist(),[[1,0,0,0,0] for i in range(len(train_data[:,0]))])
-
-                # nope:ValueError: Error when checking input: expected conv2d_1_input to have 4 dimensions, but got array with shape (3225, 1)
-                # yield ([train_data[:,0].reshape(len(train_data[:,0]))],[[1,0,0,0,0] for i in range(len(train_data[:,0]))])
-
-                # ValueError: Error when checking input: expected conv2d_1_input to have 4 dimensions, but got array with shape (3225, 1)
-                # yield (train_data[:, 0].reshape(len(train_data[:, 0])),
-                #        [[1, 0, 0, 0, 0] for i in range(len(train_data[:, 0]))])
-
-                # x_train = np.array([i[1] for i in train_data[:]]).reshape(-1, 240, 320, 3)
-                # y_train = np.array([i[0] for i in train_data[:-50]])
-                # yield (x_train,
-                #        [[1, 0, 0, 0, 0] for i in range(len(train_data[:, 0]))])
-
-                # yield(train_data[:,0],train_data[:,1])
-
-                # nope: (<class 'ValueError'>, ValueError('cannot reshape array of size 3025 into shape (240,320,3)',), <traceback object at 0x000002A6DA3DFCC8>)
-                # model.fit(train_data[:,0], train_data[:,1],
-                #           batch_size=2000,
-                #           validation_data=(train_data[:-200,0].reshape(-1,240,320,3), train_data[:-200,1].reshape(-1,5)),
-                #           shuffle=True,
-                #           verbose=1)
-
                 # test_size = len(train_data)
                 test_size = 100
 
@@ -321,15 +225,13 @@ def generate_arrays_from_folder(folder):
                 x_test = np.array([i[0] for i in train_data[-test_size:]]).reshape(-1, 240, 320, 3)
                 y_test = np.array([i[1] for i in train_data[-test_size:]]).reshape(-1,5)
 
-                # try:
                 model.fit(x_train, y_train,
                           batch_size=100,
                           validation_data=(x_test, y_test),
                           shuffle=True,
                           verbose=1)
-                # except:
-                #     print(str(e))
-                #     print(sys.exc_info())
+
+                model.save("BasicCNN-{}-epochs-{}-LR-STAGE1".format(EPOCHS, learning_rate))
 
                 print("lol")
 
@@ -340,23 +242,8 @@ def generate_arrays_from_folder(folder):
 
 
 # model.fit_generator(generate_arrays_from_folder('phase-3'),
-#                     steps_per_epoch=EPOCHS*FILE_I_END, epochs=1)
-
-# model.fit_generator(generate_arrays_from_folder('phase-3'),
-#                     steps_per_epoch=FILE_I_END, epochs=EPOCHS)
-
-# 28239
-# model.fit_generator(generate_arrays_from_folder('phase-3'),
-#                     steps_per_epoch=28000, epochs=EPOCHS)
-
-# model.fit_generator(generate_arrays_from_folder('phase-3'),
-#                     steps_per_epoch=125, epochs=EPOCHS)
-
-# model.fit_generator(generate_arrays_from_folder('phase-3'),
 #                     steps_per_epoch=5, epochs=EPOCHS)
 #
 # model.save(MODEL_NAME)
 
 generate_arrays_from_folder('phase-3')
-
-# generate_arrays_from_folder('phase-3')
