38999cd39a9234b65d1971ca4d31f7b3149b2aed
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2018-07-20 21:39:29 +0200

    Brute force playing: adding a sketch of memory reading

--- 5keys-nn/1-BruteForceWinSampling-6keys-sedf-ctrl-none.py
+++ 5keys-nn/1-BruteForceWinSampling-6keys-sedf-ctrl-none.py
@@ -12,42 +12,14 @@ import os
 # to generate alternative timing for keystroke to get around more easily
 from numpy import random
 
-
-# MoveToTheTileNextToMe(player1indexes,node)
-# rightward (0,1)
-# left (0,-1)
-# downward (1,0)
-# upward (-1,0)
-def MoveToTheTileNextToMe(playerPos, nextStepPos):
-
-    print("MoveToTheTileNextToMe:",playerPos, nextStepPos)
-    # timePress = 0.15
-    timePress = 0.10+random.randint(5)*0.01
-    # timePress = 0.10+random.randint(10)*0.01
-    # timePress = random.randint(5)*0.01
-
-    # upward
-    if(playerPos[0]>nextStepPos[0]):
-        keyboard.press('e')
-        time.sleep(timePress)
-        keyboard.release('e')
-    # downward
-    if(playerPos[0]<nextStepPos[0]):
-        keyboard.press('d')
-        time.sleep(timePress)
-        keyboard.release('d')
-    # rightward
-    if(playerPos[1]<nextStepPos[1]):
-        keyboard.press('f')
-        time.sleep(timePress)
-        keyboard.release('f')
-    # leftward
-    if (playerPos[1] > nextStepPos[1]):
-        keyboard.press('s')
-        time.sleep(timePress)
-        keyboard.release('s')
-
-    pass
+# for memory reading
+import ctypes as c
+from ctypes import wintypes as w
+from struct import *
+from time import *
+import datetime
+import sys
+import time
 
 
 # ============================================
@@ -102,6 +74,40 @@ while True:
     else:
         print('File does not exist, starting fresh!', starting_value)
         break
+
+# ==================MEMORY READING PREP======================
+# TODO: please set this or automate it
+pid = 1960
+
+k32 = c.windll.kernel32
+
+OpenProcess = k32.OpenProcess
+OpenProcess.argtypes = [w.DWORD,w.BOOL,w.DWORD]
+OpenProcess.restype = w.HANDLE
+
+ReadProcessMemory = k32.ReadProcessMemory
+ReadProcessMemory.argtypes = [w.HANDLE,w.LPCVOID,w.LPVOID,c.c_size_t,c.POINTER(c.c_size_t)]
+ReadProcessMemory.restype = w.BOOL
+PAA = 0x1F0FFF
+# PAA = 0x19F7D0
+startAddress = 0x0000000
+# startAddress = 0x4000000
+endAddress = 0x5000000
+
+ph = OpenProcess(PAA,False,int(pid)) #program handle
+
+buff = c.create_string_buffer(4)
+bufferSize = (c.sizeof(buff))
+bytesRead = c.c_ulonglong(0)
+
+listOf680 = []
+
+# 0x4 : 4 bytes steps
+# addresses_list = xrange(address,0x9000000,0x4)
+addresses_list = range(startAddress,endAddress,0x4)
+
+# ============================================================
+
 
 def main(file_name, starting_value):
     file_name = file_name
@@ -203,6 +209,21 @@ def main(file_name, starting_value):
 
             # TODO: check for variable set to one in memory
             #  when the round is restarting/ending
+
+            tmpOffset = int(0x4440B0)
+            print("tmpOffset", '%s' % hex(tmpOffset))
+
+            ReadProcessMemory(ph, c.c_void_p(tmpOffset), buff, bufferSize, c.byref(bytesRead))
+            value = unpack('I', buff)[0]
+            print("value", value)
+
+            # TODO: check if player1 is dead and not the player2:
+            # if(value == 1 and 1)
+
+            # stop the recording if it is too long (and kill the player ?)
+            if(i == 50000):
+                break
+
             i+=1
             if(i == 500):
                 roundEnded = True
