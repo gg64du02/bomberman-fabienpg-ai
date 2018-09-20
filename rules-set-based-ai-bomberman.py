320d66e2414bfbfe46df652fb5e6ac055f0a7eba
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2018-09-20 22:33:52 +0200

    Rules based ai: fixed bug in potentialPathWithinBlasts

--- rules-set-based-ai-bomberman.py
+++ rules-set-based-ai-bomberman.py
@@ -219,7 +219,7 @@ def potentialPathWithinBlasts(listOfBombs,potentialPath):
             # DONE bugfix: while ((potentialPath[xTmp, yTmp] == 1) & (isIndexesRange((xTmp, yTmp)))):
             # DONE bugfix: IndexError: index 15 is out of bounds for axis 0 with size 15
             while ((potentialPath[yTmp, xTmp] == 1) and (isIndexesRange((yTmp, xTmp))==True) or tileBombOnce ==True):
-                tileBombOnce == False
+                tileBombOnce = False
                 pathInBlasts[yTmp, xTmp] = 1
                 if (i == 0):
                     xTmp += 1
