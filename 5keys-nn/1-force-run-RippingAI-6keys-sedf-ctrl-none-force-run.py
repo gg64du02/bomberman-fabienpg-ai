3c8e2f35f5aadf95fe0c72659e15892762c42717
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-02-08 22:31:14 +0100

    Adding a script to allow the computer to run 24/7

---
+++ 5keys-nn/1-force-run-RippingAI-6keys-sedf-ctrl-none-force-run.py
@@ -0,0 +1,7 @@
+import os
+while(1):
+    try:
+        print("Issuing the script")
+        os.system('python 1-RippingAI-6keys-sedf-ctrl-none.py')
+    except:
+        print("The script crashed")
