52fe5d0a94b60db33b10ba4509c049dc6b786f65
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-02-11 01:54:16 +0100

    Bug fixing: player2 was used for target selection

--- rules-set-based-ai-bomberman.py
+++ rules-set-based-ai-bomberman.py
@@ -624,17 +624,14 @@ def oneStepToPutBomb(potentialPath,potentialPathList,
 
     if(bestBombSpotPos!=[]):
         powerups.append(bestBombSpotPos)
+        targetPosition = bestBombSpotPos
         for test in powerups:
-            dist1 = math.hypot(playerYindex - test[0], playerXindex - test[1])
-            dist2 = math.hypot(playerYindex - targetPosition[0], playerXindex - targetPosition[1])
+            dist1 = math.hypot(player1indexes[0] - test[0], player1indexes[1] - test[1])
+            dist2 = math.hypot(player1indexes[0] - targetPosition[0], player1indexes[1] - targetPosition[1])
             if(dist1<dist2):
                 targetPosition = test
             else:
                 targetPosition = player1indexes
-
-            targetPosition = min(targetPosition, test)
-
-        targetPosition = bestBombSpotPos
     else:
         targetPosition = closest_node1
         # pass
