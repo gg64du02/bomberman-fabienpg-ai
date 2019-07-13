3379aca271cd364b8db7f962d5f6bf0714d2097f
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2019-07-13 15:18:56 +0200

    Graphing logs after training (collect the relevant lines with notepad++ marks)

---
+++ 5keys-nn/run6-2/graphing_results_run6.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+loss = []
+acc = []
+val_loss = []
+val_acc = []
+with open('run6-2-lastlog-points.txt','r') as f:
+    read_data = f.read()
+    # print(read_data)
+    lol1 = read_data.split('-')
+    # lol2 = lol1.split('-')
+    print(lol1)
+    # lol3 = lol2.split('-')
+    # print(lol2)
+    for word in lol1:
+        print('word',word)
+        # if(not '-' in word):
+        if(' loss' in word):
+            loss.append(float(word.split(':')[1]))
+        if(' acc' in word):
+            lol4 = word.split('\n')
+            # print('lol4',lol4)
+            lol5 = lol4[0].split(' ')
+            # print('lol5',lol5)
+            acc.append(float(lol5[2]))
+        if(' val_loss' in word):
+            val_loss.append(float(word.split(':')[1]))
+        if(' val_acc' in word):
+            lol4 = word.split('\n')
+            print('lol4',lol4)
+            lol5 = lol4[0].split(' ')
+            print('lol5',lol5)
+            val_acc.append(float(lol5[2]))
+
+            # lol4 = word.replace('\n',' ')
+            # # val_acc.append(float(word.split(':')[1]))
+            # val_acc.append(float(lol4[0]))
+# print('loss',loss)
+plt.plot(loss)
+plt.plot(acc)
+plt.plot(val_loss)
+plt.plot(val_acc)
+plt.ylabel('some numbers')
+plt.show()
+f.closed
